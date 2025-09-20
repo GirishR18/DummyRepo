2e093f220f74ba18380dd5976f1903ceb332bc6d
GIRISH <girishrayamane@gmail.com>
2025-09-20 12:05:23 +0530

    Merge branch 'feature_branch' of https://github.com/GirishR18/DummyRepo

--- SeleniumCRMGUIFrameWork/src/main/java/imp/concepts/javBasics/Girish26TE2.java
+++
@@ -1,8 +0,0 @@
-package imp.concepts.javBasics;
-
-public class Girish26TE2 {
-	public void te2() {
-		System.out.println("Te 2 is added a method");
-	}
-
-}
---
+++ SeleniumCRMGUIFrameWork/src/main/java/imp/concepts/javBasics/TE2Class.java
@@ -0,0 +1,8 @@
+package imp.concepts.javBasics;
+
+public class TE2Class{
+
+	public void Te2Display() {
+		System.out.println("TE2 is added a method");
+	}
+}
