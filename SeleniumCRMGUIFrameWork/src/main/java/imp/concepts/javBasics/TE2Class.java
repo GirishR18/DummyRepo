a080f8f9c0e3a9b5ceae0cb979bfc302d660e622
GIRISH <girishrayamane@gmail.com>
2025-09-20 12:24:40 +0530

    TE2 changes

--- SeleniumCRMGUIFrameWork/src/main/java/imp/concepts/javBasics/TE2Class.java
+++ SeleniumCRMGUIFrameWork/src/main/java/imp/concepts/javBasics/TE2Class.java
@@ -5,4 +5,7 @@ public class TE2Class{
 	public void Te2Display() {
 		System.out.println("TE2 is added a method");
 	}
+	public void Te2Changes() {
+		System.out.println("Te2 Made changes ");
+	}
 }
